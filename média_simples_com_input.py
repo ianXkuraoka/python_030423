5467267f326e5ca67bf767327ddc7e1d22f13c9d
Ian <ian.xk9@gmail.com>
2023-04-03 09:49:02 -0300

    calculadora de média simples com input

---
+++ média_simples_com_input.py
@@ -0,0 +1,18 @@
+notas=[0,0,0,0,0]
+soma=0
+x=0
+
+while x<5:
+    notas[x]=float(input('Nota %d:'%x))
+    soma+= notas[x]
+    x+=1
+X=0
+
+while x<5:
+    print('Nota %d: %6.2f'%(x,notas[x]))
+    x+=1
+print('Média: %5.2f'%(soma/x))
+
+
+
+
